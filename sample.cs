0495321be5a7e74040ff3fc81e70ca63f2a3a20b
atshow <atshow@gmail.com>
2016-07-17 10:22:48 +0900

    作った

--- sample.cs
+++ sample.cs
@@ -1,9 +1,31 @@
 using System;
 
-public class Hello
+public class Test
 {
-  public static void Main()
+  private static int count(int valueMax, int targetCnt)
   {
-    System.Console.WriteLine("Hello, World! C#");
+    int hitCnt = 0;
+
+    for(int i = 0; i <= valueMax; i++){
+      String str = Convert.ToString(i, 2);
+      int cnt = 0;
+      foreach (char c in str)
+      {
+          if(c == '1'){cnt++;}
+      }
+
+      if(cnt == targetCnt){
+        hitCnt++;
+      }
+    }
+    return hitCnt;
+  }
+
+  static void Main(){
+    String line;
+    for(;(line=Console.ReadLine())!=null;){
+      String[] arg = line.Split(' ');
+      Console.WriteLine(count(int.Parse(arg[0]), int.Parse(arg[1])));
+    }
   }
 }
