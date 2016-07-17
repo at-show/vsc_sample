de179ca3048890d3489a20d954f2c34d3653aaa7
atshow <atshow@gmail.com>
2016-07-17 11:35:14 +0900

    S一旦保存

---
+++ S-level.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Test
+{
+  private class Point { public int X; public int Y; }
+  private static Point[64][64] map;
+
+  private static canMove(Point current, int x, int y){
+
+  }
+
+  private static void Search(Point current, List<Point> route){
+
+  }
+
+  static void Main()
+  {
+    String line;
+    for(;(line=Console.ReadLine())!=null;)
+    {
+      String[] arg = line.Split(' ');
+      //Console.WriteLine(patternCount(int.Parse(arg[0]), 0));
+    }
+  }
+}
