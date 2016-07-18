65fbd0e54a0e9554ff7efaecdcaf680014ebf356
at-show <atshow@gmail.com>
2016-07-18 15:28:22 +0900

    OK

--- S-level.cs
+++ S-level.cs
@@ -3,123 +3,131 @@ using System.Collections.Generic;
 
 public class Test
 {
-    enum direction { down, right, up, left };
     private class Point
     {
-        public int Y; public int X;
+        public int Y;
+        public int X;
         public Point(int y, int x) { Y = y; X = x; }
-    }
 
-    private static List<Point> canMove(Point current, char[,] map)
-    {
-        List<Point> list = new List<Point>();
-
-        if (map[current.Y + 1, current.X] != '#')
+        public Point Move(Point p)
         {
-            list.Add(new Point(1, 0));
-        }
-        if (map[current.Y, current.X + 1] != '#')
-        {
-            list.Add(new Point(0, 1));
-        }
-        if (map[current.Y - 1, current.X] != '#')
-        {
-            list.Add(new Point(-1, 0));
-        }
-        if (map[current.Y, current.X - 1] != '#')
-        {
-            list.Add(new Point(0, -1));
-        }
-        return list;
-    }
-
-    private static int Search(Point current, Point vec, char[,] map, int turnCnt)
-    {
-        writeMap(map, 5, 5);
-
-        // カレントがゴールなら終了
-        if (map[current.Y, current.X] == 'g')
-        {
-            return turnCnt;
-        }
-
-        // カレントを移動不可とし次を検索
-        char[,] nextMap = new char[66, 66];
-        for (int y = 0; y < 66; y++)
-        {
-            for (int x = 0; x < 66; x++)
-            {
-                nextMap[y, x] = map[y, x];
-            }
-        }
-        nextMap[current.Y, current.X] = '#';
-        List<Point> list = canMove(current, nextMap);
-        if (list.Count == 0)
-        {
-            return 9999;    // 行き詰った
-        }
-        else
-        {
-            int minTurn = 9999;
-            foreach (Point route in list)
-            {
-                int nextTurn = turnCnt;
-                if (vec != null && vec.Y != route.Y && vec.X != route.X)
-                {
-                    nextTurn++;     // 方向転換したら+1
-                }
-                int routeTurn = Search(new Point(current.Y + route.Y, current.X + route.X), route, nextMap, nextTurn);
-                if (routeTurn < minTurn)
-                {
-                    minTurn = routeTurn;
-                }
-            }
-            return minTurn;
+            return new Point(this.Y + p.Y, this.X + p.X);
         }
     }
 
-    private static void writeMap(char[,] map, int row, int col)
+    private static bool canMove(char[,] map, int row, int col, Point address)
     {
-        /*
-        Console.WriteLine("----------");
+        if(address.Y < 0 || address.Y >= row || address.X < 0 || address.X >= col)
+        {
+            return false;
+        }
+
+        if(map[address.Y, address.X] == '#')
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private static int Search(int row, int col, char[,] map)
+    {
+        writeMap(map, row, col);
+        List<Point> direction = new List<Point>{ new Point(1,0), new Point(0,1), new Point(-1,0), new Point(0,-1)};
+        Queue<Point> target = new Queue<Point>();
+        int[,] mapCount = new int[row, col];
         for (int y = 0; y < row; y++)
         {
             for (int x = 0; x < col; x++)
             {
-                Console.Write(map[y, x]);
+                mapCount[y, x] = 9999;  // とりあえずMAX
             }
-            Console.WriteLine("");
         }
-        */
+
+        // スタート地点設定(一律処理のため-1スタート)
+        mapCount[0,0] = -1;
+        target.Enqueue(new Point(0,0));
+
+        while(target.Count > 0)
+        {
+            Point current = target.Dequeue();
+
+            if(current.Y == (row - 1) && current.X == (col - 1))
+            {
+                // ゴールだった
+                // スタートとゴールが同じだった場合-1が返ってしまう
+                return (mapCount[current.Y, current.X] == -1 ? 0 : mapCount[current.Y, current.X]);
+            }
+
+            // 上下左右にturnを設定
+            int curTurn = mapCount[current.Y, current.X];
+            foreach (Point dir in direction)
+            {
+                Point tp = current.Move(dir);
+                while(canMove(map, row, col, tp))
+                {
+                    if(mapCount[tp.Y, tp.X] > curTurn + 1)
+                    {
+                        // 最小ターンでだどり着いたのでキューに設定
+                        mapCount[tp.Y, tp.X] = curTurn + 1;
+                        target.Enqueue(tp);
+                    }
+                    tp = tp.Move(dir);
+                }
+            }
+            writeMap(mapCount, row, col);
+        }
+
+        return -1;
     }
 
+    private static void writeMap(char[,] map, int row, int col)
+    {
+        //Console.WriteLine("----------");
+        //for (int y = 0; y < row; y++)
+        //{
+        //    for (int x = 0; x < col; x++)
+        //    {
+        //        Console.Write(map[y, x]);
+        //    }
+        //    Console.WriteLine("");
+        //}
+    }
+    private static void writeMap(int[,] map, int row, int col)
+    {
+        //Console.WriteLine("***********");
+        //for (int y = 0; y < row; y++)
+        //{
+        //    for (int x = 0; x < col; x++)
+        //    {
+        //        Console.Write(map[y, x] + ",");
+        //    }
+        //    Console.WriteLine("");
+        //}
+    }
     static void Main()
     {
-        char[,] map = new char[66, 66];
-        for (int y = 0; y < 66; y++)
-        {
-            for (int x = 0; x < 66; x++)
-            {
-                map[y, x] = '#';
-            }
-        }
-
+        char[,] map = null;
         String line;
+        int row = 0;
+        int col = 0;
+
         if ((line = Console.ReadLine()) != null)
         {
             String[] arg = line.Split(' ');
-            for (int y = 0; y < int.Parse(arg[0]); y++)
+            row = int.Parse(arg[0]);
+            col = int.Parse(arg[1]);
+            map = new char[row, col];
+ 
+            for (int y = 0; y < row; y++)
             {
                 line = Console.ReadLine();
-                for (int x = 0; x < int.Parse(arg[1]); x++)
+                for (int x = 0; x < col; x++)
                 {
-                    map[y + 1, x + 1] = line[x];
+                    map[y, x] = line[x];
                 }
             }
-            map[1, 1] = 's';
-            map[int.Parse(arg[0]), int.Parse(arg[1])] = 'g';
         }
         
-        Console.WriteLine(Search(new Point(1, 1), null, map, 0));
+        Console.WriteLine(Search(row, col, map));
     }
 }
